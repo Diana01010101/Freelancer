eb2273d6d7fc09d48b7d0f5bbb1e1800e5835014
Diana test <diana.kapralova@mail.ru>
2023-10-28 22:32:08 +0300

    change Freelancer

--- src/test/java/ru/netology/services/FreelancerServiceTest.java
+++ src/test/java/ru/netology/services/FreelancerServiceTest.java
@@ -2,31 +2,39 @@ package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FreelancerServiceTest {
 
-    @Test
-    public void testCalculate1() {
+    @ParameterizedTest
+//    @CsvSource({
+//            "3,10000,3000,20000",
+//            "2,100000,60000,150000"
+//    })
+    @CsvFileSource(files="src/test/resources/freelancer.csv")
+    public void testCalculate1(int expected, int income, int expenses, int threshold) {
         //доходы 10, расходы 3, порог 20
         FreelancerService service = new FreelancerService();
 
-        int actual = service.calculate(10_000, 3_000, 20_000);
-        int expected = 3;
+        int actual = service.calculate(income, expenses, threshold);
+        //int expected = 3;
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    public void testCalculate2() {
+//    @Test
+//    public void testCalculate2() {
         //доходы 10, расходы 3, порог 20
-        FreelancerService service = new FreelancerService();
+//        FreelancerService service = new FreelancerService();
 
-        int actual = service.calculate(100_000, 60_000, 150_000);
-        int expected = 2;
+//        int actual = service.calculate(100_000, 60_000, 150_000);
+//        int expected = 2;
 
-        Assertions.assertEquals(expected, actual);
-    }
+//        Assertions.assertEquals(expected, actual);
+//    }
 
 }
